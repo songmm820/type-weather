b4e04761ab0135e1aa220f039b9fd2c35f1406f9
SongMing <mmsong@yeah.net>
2025-08-21 15:36:32 +0800

    fix：客户端通知配置

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -42,6 +42,7 @@ pub fn run() {
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(
             tauri_plugin_log::Builder::new()
                 .target(tauri_plugin_log::Target::new(
